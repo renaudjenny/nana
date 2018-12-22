08a6d0138288ad94350f92abe9e4b0cc2223d898
Renaud Jenny <renaud.jenny@gmail.com>
2018-12-22 11:57:23 +0100

    ui: improve UI design for the main page title

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 
 class MainPage extends React.Component {
   render () {
     return (
-      <Typography variant='title' component='h1'>Posts list</Typography>
+      <Grid container spacing={24} direction='column' alignItems='center' justify='center'>
+        <Grid item>
+          <Typography variant='title' component='h1'>Posts list</Typography>
+        </Grid>
+      </Grid>
     )
   }
 }
