ef0b694e0a0fe11a724256e2ca2f23455f59ef05
Renaud Jenny <renaud.jenny@gmail.com>
2018-12-22 11:56:21 +0100

    feat: use MainPage component as the app main page

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
+import ReactRouter from './components/ReactRouter'
 
-ReactDOM.render(<h1>Nana</h1>, document.getElementById('root'))
+ReactDOM.render(<ReactRouter />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
