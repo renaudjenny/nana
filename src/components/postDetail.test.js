dd4ff021b2e08117532f897a640c458fd6d55ea7
Renaud Jenny <renaud.jenny@gmail.com>
2018-12-25 18:22:27 +0100

    test(post detail): add missing test when page data are loaded

--- src/components/postDetail.test.js
+++ src/components/postDetail.test.js
@@ -9,6 +9,7 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 
 describe('Given I\'m on a Post Detail', () => {
   let wrapper
+  let postDetail
 
   beforeEach(() => {
     wrapper = mount(
@@ -16,6 +17,7 @@ describe('Given I\'m on a Post Detail', () => {
         <PostDetail />
       </MemoryRouter>
     )
+    postDetail = wrapper.find(PostDetail).instance()
   })
 
   afterEach(() => {
@@ -32,6 +34,42 @@ describe('Given I\'m on a Post Detail', () => {
     test('Then I see an indicator shows data are loading', () => {
       const indicator = wrapper.find(CircularProgress)
       expect(indicator).toHaveLength(1)
+    })
+  })
+
+  describe('When the data are loaded', () => {
+    beforeAll(() => {
+      Enzyme.configure({ adapter: new Adapter() })
+      const mock = new MockAdapter(axios)
+      const data = {
+        id: 1,
+        name: 'Mario Bros',
+        username: 'Super Mario',
+        email: 'mario@nintendo.com',
+        address: {
+          street: 'Koopa Street',
+          suite: 'Apt. 123',
+          city: 'Mushroom Kinkdom',
+          zipcode: '12345-6789'
+        },
+        phone: '1-770-736-8031 x56442',
+        website: 'nintendo.com',
+        company: {
+          name: 'Nintendo corp.',
+          catchPhrase: 'Ohhhh. Mamma Mia!',
+          bs: 'plumber brothers'
+        }
+      }
+      mock.onGet('http://jsonplaceholder.typicode.com/posts/1').reply(200)
+    })
+
+    test('Then the loading indicator is hidden', () => {
+      expect.assertions(1)
+      return postDetail.postLoadPromise.then(() => {
+        wrapper.update()
+        const indicator = wrapper.find(CircularProgress)
+        expect(indicator).toHaveLength(0)
+      })
     })
   })
 })
